62a5d0fe4f7c104f4c40a59672e32cceb4d219a2
Talkave <matburz94@gmail.com>
2025-08-07 11:56:46 +0200

    day 11 complete

--- Advent of code/Advent of code 11.py
+++ Advent of code/Advent of code 11.py
@@ -26,7 +26,7 @@
 
 test_input = "abcdefgh" 
 mode = "input"
-# mode = "test_input"
+#mode = "test_input"
 
 if mode == "input":
     input_path = "./Advent of code/input_day11.txt"
@@ -37,7 +37,50 @@ elif mode == "test_input":
     input = test_input
     print("MODE = TEST")
 
-ord('a') = 97 # podobne zadanie do dzień 5.
-ord('z') = 122 # wartości ascii alfabetu do iterowania 
+# print(ord('i')) # 105
+# print(ord('o')) # 111
+# print(ord('l')) # 108
+# ord('a') = 97 # podobne zadanie do dzień 5.
+# ord('z') = 122 # wartości ascii alfabetu do iterowania 
+# chr(122) = 'z' # odwrotne działanie
+def if_banned(s):
+    # warunek 1: sprawdzamy pary
+    i = 0
+    pairs = 0
+    while i < len(s) - 1: 
+        if s[i] == s[i+1]:
+            pairs += 1
+            i += 2    
+        else:
+            i += 1
+    if pairs < 2 :
+        return True
+    # warunek 2: sprawdzamy czy jest i o l
 
+    if any(char in ['i','o','l'] for char in s):
+        return True
+    # warunek 3: sprawdzamy czy jest straight 'abc' 'bcd' itd.
+    for j in range(len(s) - 2): 
+        if ord(s[j+2]) - ord(s[j+1]) == 1  and ord(s[j+1]) - ord(s[j]) == 1:
+            return False
+    return True
 
+def pass_generator(password):
+    password = list(password) # zamień na listę bo w stringach nie da się zamieniać znaków
+    while True:
+        i = len(password) - 1 # inicjujemy iterator od końca słowa
+        while i >= 0:
+            new_ord = ord(password[i]) + 1 # dodajemy do ord + 1 żeby przeskakiwać po alfabecie
+            if new_ord > 122: # jeśli przeskoczyłeś 'z':
+                password[i] = 'a' # w tym miejscu wstaw 'a', można było dać chr(97) ale na chuj jak to samo daje 'a'
+                i -= 1 # idziemy w lewo po słowie
+            else:
+                password[i] = chr(new_ord) # w przeciwnym wypadku w tym miejscu wstaw nowy ord z linijki 73, czyli z 'a' zrob 'b' , z 'b' 'c' itd. aż do z
+                break # wyjdź z pętli bo zamieniłeś iterowany znak na nowy
+        new_password = ''.join(password) # zlep z powrotem listę w jednego stringa
+        if not if_banned(new_password): # jeśli nie jest zbanowane nowe haslo to je zwróć
+            return new_password
+        
+        password = list(new_password) # do kolejnej iteracji 
+
+print(pass_generator("hxbxxyzz"))
